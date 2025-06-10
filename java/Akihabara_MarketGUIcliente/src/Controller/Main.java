ea086002e7f7cc0dc7daa9f8084d201d7a20d55d
rafamedina <rafael.medinaayuso24@campusfp.es>
2025-06-10 10:13:00 +0200

    Instalacion de flatlaf

--- java/Akihabara_MarketGUIcliente/src/Controller/Main.java
+++ java/Akihabara_MarketGUIcliente/src/Controller/Main.java
@@ -1,14 +1,24 @@
 package Controller;
+
+import javax.swing.UIManager;
+import javax.swing.SwingUtilities;
+
 import GUI.GUIGeneral;
-import Model.LlmService;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		
-		GUIGeneral controller = new GUIGeneral();
-		controller.setVisible(true);
-	}
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            System.err.println("No se pudo aplicar FlatLaf: " + e.getMessage());
+        }
 
+        SwingUtilities.invokeLater(() -> {
+            GUIGeneral controller = new GUIGeneral();
+            controller.setVisible(true);
+        });
+    }
 }
